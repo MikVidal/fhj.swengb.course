6fa84a6fcd487b54852a35fb386276d010aaa535
Hector M Vidal <hector.vidal.dura@gmail.com>
2015-11-06 21:57:41 +0100

    first commit

--- material/src/main/scala/fhj/swengb/lectures/l03/JavaFxHelloworld.scala
+++ material/src/main/scala/fhj/swengb/lectures/l03/JavaFxHelloworld.scala
@@ -6,6 +6,7 @@ import javafx.application.Application
 import javafx.fxml.{FXMLLoader, Initializable}
 import javafx.scene.{Parent, Scene}
 import javafx.stage.Stage
+import javafx.scene.image.Image;
 
 import scala.util.control.NonFatal
 
@@ -29,6 +30,7 @@ class JavaFxHelloworld extends javafx.application.Application {
   override def start(stage: Stage): Unit =
     try {
       stage.setTitle("FXCalculator")
+      stage.getIcons().add(new Image("file:Calculator-icon.png"));
       loader.load[Parent]() // side effect
       val scene = new Scene(loader.getRoot[Parent])
       stage.setScene(scene)
